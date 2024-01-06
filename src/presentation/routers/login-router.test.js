3cb4b10a3b08074b1e759477ba6e7feb7092aa72
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-01-06 11:22:19 -0300

    Ensure LoginRouter return 500 if an invalid httpRequest id provided

--- src/presentation/routers/login-router.test.js
+++ src/presentation/routers/login-router.test.js
@@ -1,5 +1,10 @@
 class LoginRouter {
   async route (httpRequest) {
+    if (!httpRequest || !httpRequest.body) {
+      return {
+        statusCode: 500
+      }
+    }
     const { email, password } = httpRequest.body
     if (!email || !password) {
       return {
@@ -30,5 +35,19 @@ describe('Login router', () => {
     }
     const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 500 if no httpRequest is provided', async () => {
+    const sut = new LoginRouter()
+
+    const httpResponse = await sut.route()
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('Should return 500 if httpRequest has no body ', async () => {
+    const sut = new LoginRouter()
+    const httpRequest = {}
+    const httpResponse = await sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
   })
 })
