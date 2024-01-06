4d013286baa6fcc62435625c62b241683f83e61f
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-01-06 12:12:36 -0300

    Move production code to their own files

---
+++ src/presentation/helpers/HttpResponse.js
@@ -0,0 +1,13 @@
+const MissingParamError = require('./MissingParamError')
+
+class HttpResponse {
+  static badRequest (paramName) {
+    return { statusCode: 400, body: new MissingParamError(paramName) }
+  }
+
+  static serverError () {
+    return { statusCode: 500 }
+  }
+}
+
+module.exports = HttpResponse
---
+++ src/presentation/routers/LoginRouter.js
@@ -0,0 +1,19 @@
+const HttpResponse = require('../helpers/HttpResponse')
+
+class LoginRouter {
+  async route (httpRequest) {
+    if (!httpRequest || !httpRequest.body) {
+      return HttpResponse.serverError()
+    }
+    const { email, password } = httpRequest.body
+    if (!email) {
+      return HttpResponse.badRequest('Email')
+    }
+
+    if (!password) {
+      return HttpResponse.badRequest('Password')
+    }
+  }
+}
+
+module.exports = LoginRouter
