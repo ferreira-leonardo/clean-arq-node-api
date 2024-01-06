ea00f9458d394619f550371d0a12844d31183123
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-01-06 11:01:24 -0300

    Ensure LoginRouter 400 if no email is provided

---
+++ src/presentation/routers/login-router.test.js
@@ -0,0 +1,22 @@
+class LoginRouter {
+  async route (httpRequest) {
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400
+      }
+    }
+  }
+}
+
+describe('Login router', () => {
+  test('Should return 400 if no email is provided', async () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        password: 'any-password'
+      }
+    }
+    const httpResponse = await sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
